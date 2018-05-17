34d21575610953cb1dbe8fb687647571330308c3
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-05-17 14:07:11 -0500

    SE AGREGAN MODIFICACIONES EN LA VERSION 1 Y 2 Y SE AGREGA LA VERSION 3

--- js/app.js
+++ js/app.js
@@ -5,16 +5,22 @@
     boton.addEventListener('click',function(){
         //Se toma el Comentario que Ingresa el Usuario en el Text Area 
         var sectionContainerComment=document.getElementById('area-para-cometarios');
-        var comment=document.getElementById('comment').value;
+        var comment=document.getElementById('comment').value
+        if(comment == null) {
+            var deshabilitado="";
+            boton.disabled=true;
+            deshabilitado="deshabilitad";
+            boton.className=deshabilitado;
+        }else if(comment.length>=0 && comment.length<140){
+        boton.disabled=false;
         var valueComment=document.createTextNode(comment);
-        //Se creara el elemento que contendra el comentario
-        var divComment= document.createElement('div');
+        var divComment= document.createElement('div');  //Se creara el elemento que contendra el comentario
         divComment.setAttribute('id','containerComment');
         divComment.setAttribute('class','formatContainerComment');
         divComment.appendChild(valueComment);
         sectionContainerComment.appendChild(divComment);
         document.getElementById('comment').value="";
-        //Se asignan los comentarios a la Seccion donde se colocaran
+        }//se cierra el else if 
     });
 
     //version  2
@@ -22,23 +28,22 @@
     var contador= function(){
         var msg = document.getElementById("comment");//se mandan traer al js el text area del comentario añadido
         var charRest = document.getElementById("CaracteresRestantes");//se manda a traer del html el elemento parrafo creado para colocar el contador de caracteres 
-            LimiteCaracteres = 140;//limite maximo de caracteres que se deben permitir 
-            charRest.innerHTML = LimiteCaracteres;
-            msg.onkeydown = function(){ //se activa cuando el usuario presiona una tecla 
-                setTimeout(function(){ //activa el tiempo en el que va a ejecutarse en milisegundos 
-                charRest.innerHTML = LimiteCaracteres - msg.value.length;
-                var estilo=''; 
+        LimiteCaracteres = 140;//limite maximo de caracteres que se deben permitir 
+        charRest.innerHTML = LimiteCaracteres;
+        msg.onkeydown = function(){ //se activa cuando el usuario presiona una tecla 
+            charRest.innerHTML = LimiteCaracteres - msg.value.length;
+            var estilo=''; 
                 if(msg.value.length >= 130){
                     estilo="alarma";
                 } else if(msg.value.length >= 120){
                     estilo="warning";
-                } else if(msg.value.length==0){
+                } else if(msg.value.length > 140){
                     var botonDesHabilitar=document.getElementById('btn');
                     botonDesHabilitar.disabled=true;
                 }
-                charRest.className = estilo;
-            }, 1); 
-            };
-    }
+                charRest.className = estilo;//para asignar los estilos
+            
+            };//cierra la funcion onkeydown
+    }//cierra la funcion contador 
 
     contador();
